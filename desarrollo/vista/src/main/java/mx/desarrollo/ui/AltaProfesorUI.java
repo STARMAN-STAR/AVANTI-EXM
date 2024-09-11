bb20974d057e92e68c0357856ccaa9b05aca8367
MiguelAnCG <167253911+MiguelAnCG@users.noreply.github.com>
2024-09-11 01:40:52 -0700

    Manejador del xhtml Alta de profesor
    Cree el manejador para la alta de los profesores, en este java se manejaran todos los movimientos realizados en el xhtml altaProfesor

---
+++ desarrollo/vista/src/main/java/mx/desarrollo/ui/AltaProfesorUI.java
@@ -0,0 +1,71 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mx.desarrollo.ui;
+
+import javax.inject.Named;
+import javax.enterprise.context.SessionScoped;
+import java.io.Serializable;
+
+/**
+ *
+ * @author PC
+ */
+@Named(value = "altaProfesor")
+@SessionScoped
+public class AltaProfesorUI implements Serializable {
+    private String nombres;
+    private String apellidos;
+    private String rfc;
+    private String[] unidadesDeAprendizaje;
+    
+    public void subirProfesor() {
+        
+    }
+    
+    public String getNombres() {
+        return nombres;
+    }
+
+    public void setNombres(String nombres) {
+        this.nombres = nombres;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    public String getRfc() {
+        return rfc;
+    }
+
+    public void setRfc(String rfc) {
+        this.rfc = rfc;
+    }
+
+    public String[] getUnidadesDeAprendizaje() {
+        return unidadesDeAprendizaje;
+    }
+
+    public void setUnidadesDeAprendizaje(String[] unidadesDeAprendizaje) {
+        this.unidadesDeAprendizaje = unidadesDeAprendizaje;
+    }
+    
+    public AltaProfesorUI() {
+        
+    }
+    
+    public void AltaProfesorUI(String nombres, String apellidos, String rfc, String[] unidadesDeAprendizaje) {
+        this.nombres = nombres;
+        this.apellidos = apellidos;
+        this.rfc = rfc;
+        this.unidadesDeAprendizaje = unidadesDeAprendizaje;
+    }
+    
+}
