af3f656b5c5108fc366dd171bbbe536b42429f5d
Luis Gazcón <luiselgazcon@gmail.com>
2024-09-12 13:08:13 -0700

    feat: create application daos

---
+++ desarrollo/persistencia/src/main/java/mx/desarrollo/DAO/AdministratorDAO.java
@@ -0,0 +1,7 @@
+package mx.desarrollo.DAO;
+
+import mx.desarrollo.entidad.Administrator;
+import mx.desarrollo.persistencia.AbstractDAO;
+
+public class AdministratorDAO extends AbstractDAO<Integer, Administrator> {
+}
