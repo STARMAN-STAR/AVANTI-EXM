08869760aa60019152cbd1676a35071cb6147518
STARMAN <francisco.villaman@uabc.edu.mx>
2024-09-15 23:02:09 -0700

    Modificacion en capa web

--- desarrollo/entidad/target/generated-sources/annotations/mx/desarrollo/entidad/Learningunit_.java
+++ desarrollo/entidad/target/generated-sources/annotations/mx/desarrollo/entidad/Learningunit_.java
@@ -6,7 +6,7 @@ import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 import mx.desarrollo.entidad.Teacherlearningunit;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2024-09-15T21:59:18")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2024-09-15T22:38:17")
 @StaticMetamodel(Learningunit.class)
 public class Learningunit_ { 
 
